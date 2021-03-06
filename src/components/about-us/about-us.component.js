1f5614c36239a88c5a09596c6f3c9bf76643f28e
PaulSoumik <soumikpaul1999@gmail.com>
2021-03-06 16:22:44 +0530

    new update

--- src/components/about-us/about-us.component.js
+++ src/components/about-us/about-us.component.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import { Link, Redirect, Switch, Route } from "react-router-dom";
 import "./aboutus.css"
 import "../common.css"
-import Videojs from './videoplayer.js';
-import VideoPlayer from "./videoplayer";
 import  poster from "../../media/images/initpic.png";
 import Videomp4 from "../../media/video/ieeetrailer.mp4";
 import Videowebm from "../../media/video/ieeetrailer.webm";
+import VideoApp from "./Videoapp.js";
 
 const videoJsOptions= {
 				  autoplay: false,
@@ -17,6 +16,10 @@ const videoJsOptions= {
 				    type: "video/webm"
 				  }]
 				}
+const VideAppoptions = {
+	src: Videowebm,
+	poster: poster
+}
 
 export default class AboutUs extends Component {
 	  constructor(props) {
@@ -39,16 +42,17 @@ export default class AboutUs extends Component {
 	  	 return(
 	  	 	<div className="main-container about-us row p-3">
 	  	 		<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-		  	 	<h2 className="special-headers"> About Us </h2>  
-		  	 	<p className="p-2 pl-3">	The IEEE Student Branch , NIT Durgapur is a society of enthusiasts aimed at promoting research-related
-	            activities in the campus. We are a direct handshake to IEEE, an international body that allows countless
-	            young researchers the opportunity to present &amp; publish their innovations every year. Comprising of
-	            bright researchers, developers, speakers, and other contributors we are a society that welcomes in the era
-	            of better research prospects, on the campus.
-	            </p>
+			  	 	<h2 className="special-headers"> About Us </h2>  
+			  	 	<p className="p-2 pl-3">	The IEEE Student Branch , NIT Durgapur is a society of enthusiasts aimed at promoting research-related
+		            activities in the campus. We are a direct handshake to IEEE, an international body that allows countless
+		            young researchers the opportunity to present &amp; publish their innovations every year. Comprising of
+		            bright researchers, developers, speakers, and other contributors we are a society that welcomes in the era
+		            of better research prospects, on the campus.
+		            </p>
 	  	 		</div>
 	  	 		<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 video-container">
-	  	 			<div className="video-wrapper"> <Videojs {...videoJsOptions} /> </div>
+	  	 			
+	  	 			<div className="video-wrapper "> <VideoApp {...VideAppoptions} /> </div>
 	  	 		</div>
 	  	 	</div>
 	  	 )
